5498ed5db47b66e6a6d534b951ab83ddb2c076aa
adalsteinn95 <adalsteinn95@gmail.com>
2018-05-15 15:32:44 +0000

    react router setup and routing setup

--- mtgflavortextapp/src/App.js
+++ mtgflavortextapp/src/App.js
@@ -1,19 +1,26 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Helmet from "react-helmet";
+import { Switch, Route } from "react-router-dom";
+
+import Home from "./routes/home";
+import NotFound from "./routes/not-found";
+
+/* todo fleiri routes */
+
+import "./App.css";
 
 class App extends Component {
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <main className="main">
+        <div className="main__content">
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route component={NotFound} />
+          </Switch>
+        </div>
+      </main>
     );
   }
 }
